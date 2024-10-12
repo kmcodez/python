a3e73b7cb042c5d73457bea3c117a3ffb837634d
kmcodez <mkrkmpi@gmail.com>
2024-10-12 13:57:54 +0530

    updated the files

--- tutorial_1/2_module/messages.py
+++
@@ -1,6 +0,0 @@
-def hello():
-    print("Hello! Have a nice day!")
-
-
-def bye():
-    print("Bye! have a wonderful time!")
--- tutorial_1/5_advanced/12_if_name==_main/module_two.py
+++
@@ -1,6 +0,0 @@
-import module_one
-
-if __name__ == '__main__':
-    print('running module directly')
-else:
-    print('running module indirectly')
